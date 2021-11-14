5cf044e43bad85ba6fdc8ef63ce2af6d5b2e9ef7
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-14 21:30:42 +0300

    added ICityRepository

---
+++ 5.Hafta/OopApp/OopApp/DataAccess/ICityRepository.cs
@@ -0,0 +1,10 @@
+﻿using OopApp.Entities;
+using System.Collections.Generic;
+
+namespace OopApp.DataAccess
+{
+    public interface ICityRepository : IAppRepository<City>
+    {
+        List<CityDetailDto> GetCityDetails();
+    }
+}
