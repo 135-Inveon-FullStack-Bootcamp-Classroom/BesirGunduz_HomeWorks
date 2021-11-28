04bf1fa7c62c7123286f717963db9daaa4835665
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-28 23:05:35 +0300

    update Director

--- 6.Hafta/MovieStore.API/MovieStore.API/Entities/Director.cs
+++ 6.Hafta/MovieStore.API/MovieStore.API/Entities/Director.cs
@@ -7,6 +7,6 @@ namespace MovieStore.API.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public List<Movie> Movies { get; set; }
+        public virtual ICollection<Movie> Movies { get; set; }
     }
 }
